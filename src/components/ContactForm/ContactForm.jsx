ca4716dce09fe8c4c638abc6dfd495cb60e9f8ad
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-25 15:57:38 +0100

    finally

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,9 +8,10 @@ const ContactSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "Too Short!")
     .max(50, "Too Long!")
-    .required("Required"),
+    .required("Required")
+    .matches(/^[A-Za-zА-Яа-яЄєІіЇїҐґ-\s]+$/, "Invalid format"),
   number: Yup.string()
-    .matches(/^\+?\d+$/, "Invalid format")
+    .matches(/^[\+]?(\d{1,})((-|\s)?\d{1,})*$/, "Invalid format")
     .min(3, "Too Short!")
     .max(16, "Too Long!")
     .required("Required"),
@@ -25,15 +26,18 @@ const ContactForm = ({ onAdd }) => {
   const nameFieldId = useId();
   const numberFieldId = useId();
 
+  const formatPhoneNumber = (value) => {
+    return value
+      .replace(/(\d{3})(\d{2})(\d{2})$/, "$1-$2-$3") // Формат XXX-XX-XX
+      .replace(/(\d{3})(\d{2})$/, "$1-$2"); // Формат XXX-XX, якщо введення неповне
+  };
+
   const handleSubmit = (values, actions) => {
     onAdd({
       id: nanoid(),
       name: values.name,
       number: values.number,
     });
-
-    // console.log(values.name);
-    // console.log(values.number);
 
     actions.resetForm();
   };
@@ -44,28 +48,39 @@ const ContactForm = ({ onAdd }) => {
       onSubmit={handleSubmit}
       validationSchema={ContactSchema}
     >
-      <Form className={s.form}>
-        <div className={s.labelWraper}>
-          <label htmlFor={nameFieldId}>Name</label>
-          <Field className={s.input} type="text" name="name" id={nameFieldId} />
-          <ErrorMessage className={s.span} name="name" component="span" />
-        </div>
+      {({ setFieldValue }) => (
+        <Form className={s.form}>
+          <div className={s.labelWraper}>
+            <label htmlFor={nameFieldId}>Name</label>
+            <Field
+              className={s.input}
+              type="text"
+              name="name"
+              id={nameFieldId}
+            />
+            <ErrorMessage className={s.span} name="name" component="span" />
+          </div>
 
-        <div className={s.labelWraper}>
-          <label htmlFor={numberFieldId}>Number</label>
-          <Field
-            className={s.input}
-            type="tel"
-            name="number"
-            id={numberFieldId}
-          />
-          <ErrorMessage className={s.span} name="number" component="span" />
-        </div>
+          <div className={s.labelWraper}>
+            <label htmlFor={numberFieldId}>Number</label>
+            <Field
+              className={s.input}
+              type="tel"
+              name="number"
+              id={numberFieldId}
+              onChange={(e) => {
+                const formattedValue = formatPhoneNumber(e.target.value);
+                setFieldValue("number", formattedValue); // Оновлюємо значення поля
+              }}
+            />
+            <ErrorMessage className={s.span} name="number" component="span" />
+          </div>
 
-        <button className={s.button} type="submit">
-          Add contact
-        </button>
-      </Form>
+          <button className={s.button} type="submit">
+            Add contact
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
